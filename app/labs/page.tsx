5de14ba3316492493baccd6e7f54d168aa8983b2
Alexander Kylander-Kreiner <kylander05@gmail.com>
2026-03-28 10:36:08 -0400

    lab 5 commit

--- app/labs/page.tsx
+++ app/labs/page.tsx
@@ -21,6 +21,10 @@ export default function labs() {
          <Link href="/labs/lab4" id="wd-lab4-link">
            Lab 4: State Management </Link>
        </li>
+        <li>
+         <Link href="/labs/lab5" id="wd-lab5-link">
+           Lab 5: React Components </Link>
+       </li>
        <li>
           <Link href="/" id="wd-kambaz-link">
            Kambaz </Link>
